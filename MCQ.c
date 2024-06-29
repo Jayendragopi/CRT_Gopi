583f4c034454a9c908e81ec4351fb13125208e54
Jayendragopi <121372464+Jayendragopi@users.noreply.github.com>
2024-06-29 16:10:28 +0530

    Add files via upload

---
+++ MCQ.c
@@ -0,0 +1,42 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    /*int x;
+    for(x=-1;x<=10;x++){
+        if(x<5)
+             continue;
+       else
+            break;
+      printf("hi");
+      x++;
+    }*/ 
+    
+   /* int i=0;
+    for(;i<=5;i++);
+       printf("%d",i);//6*/
+       
+       
+      /*int a=500,b=100,c;
+      if(!(a>=400))
+          b=300;
+      //c=200;
+      printf("b=%d  c=%d",b,c);//b=100,c=32456(garbage value)*/
+      
+      /*int x=3;
+      int y=3.0;
+      if(x==y)
+         printf("yes");
+    else
+       printf("no");*/
+       
+       
+      /* char ch;
+       if(ch=printf(""))
+           printf("single\n");
+       else
+          printf("not single");*/
+          
+      
+      
+    return 0;
